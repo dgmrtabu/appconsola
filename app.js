c4a780aeaf88ba313c1b46da24982ead60de9be6
DAVID GABRIEL MONTAÑO ROCHA <david.montano@cns.gob.bo>
2021-04-21 16:21:24 -0400

    C-47 localcns

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ const { monstrarMenu, pausa } = require('./helpers/mensajes');
 console.clear();
 const main = async() => {
     console.log('Hola mundo');
-    monstrarMenu();
-    pausa();
+
+    let opt = '';
+
+    do {
+        opt = await monstrarMenu();
+        console.log({ opt });
+
+        if (opt !== '0') await pausa();
+
+    } while (opt !== '0');
+
+    //pausa();
 }
 
 main();
--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,37 +1,52 @@
+const { rejects } = require('assert');
+const { resolve } = require('path');
+
 require('colors');
 
 const monstrarMenu = () => {
-    console.clear();
-    console.log('=========================='.green);
-    console.log('   Selecccion una opción'.green);
-    console.log('==========================\n'.green)
 
-    console.log(`${ '1.'.green} Crear tarea`);
-    console.log(`${ '2.'.green} Listar tarea`);
-    console.log(`${ '3.'.green} Listar tareas completadas`);
-    console.log(`${ '4.'.green} Listar tareas pendientes`);
-    console.log(`${ '5.'.green} Completar tarea(s)`);
-    console.log(`${ '6.'.green} Borrar tarea`);
-    console.log(`${ '0.'.green} Salir \n`);
+    return new Promise((resolve, rejects) => {
 
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+        console.clear();
+        console.log('=========================='.green);
+        console.log('   Selecccion una opción'.green);
+        console.log('==========================\n'.green)
 
-    readline.question('Seleccione una opción: ', (opt) => {
-        readline.close();
+        console.log(`${ '1.'.green} Crear tarea`);
+        console.log(`${ '2.'.green} Listar tarea`);
+        console.log(`${ '3.'.green} Listar tareas completadas`);
+        console.log(`${ '4.'.green} Listar tareas pendientes`);
+        console.log(`${ '5.'.green} Completar tarea(s)`);
+        console.log(`${ '6.'.green} Borrar tarea`);
+        console.log(`${ '0.'.green} Salir \n`);
+
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        readline.question('Seleccione una opción: ', (opt) => {
+            readline.close();
+            resolve(opt);
+        });
+
     });
 };
 
 const pausa = () => {
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
 
-    readline.question(`\n Presione ${'ENTER'.green} para continuar\n`, (opt) => {
-        readline.close();
+    return new Promise((resolve, rejects) => {
+
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        readline.question(`\n Presione ${'ENTER'.green} para continuar\n`, (opt) => {
+            readline.close();
+            resolve();
+        });
+
     });
 }
 
